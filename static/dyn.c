1093c8bd7730128c06f46c780c78fda7f0ce9001
Nazneen Sheikh <71650072+sheikhnazneen@users.noreply.github.com>
2024-12-16 16:46:14 +0530

    Add files via upload

---
+++ static/dyn.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+
+dev_t dev = 0; // dynamically  giving major number
+
+static int __init mod_init(void)
+{
+    alloc_chrdev_region(&dev,0, 1, "charDriver");
+    printk(KERN_INFO "\nMajor = %d Minor = %d\n", MAJOR(dev), MINOR(dev));
+    printk(KERN_INFO "\nKernel char Module Inserted Successfully...\n");
+    return 0;
+}
+
+static void __exit mod_exit(void)
+{
+    unregister_chrdev_region(dev, 1);
+    printk(KERN_INFO "\nKernerl char Module Removed Successfully..\n");
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Priyanshu");
+MODULE_DESCRIPTION("Program For Create char module with->MAJOR AND MINOR");
---
+++ static/modules.order
@@ -0,0 +1,1 @@
+/home/priyanshu/linuxdriver/static/mmst.o
